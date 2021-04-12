e286ffbd2188cff5d9948a6618c13e1375d4b4c2
Dima <menshikov.dmitry@gmail.com>
2021-04-12 14:36:23 +0300

    minor mistake

--- crypto_monitor.py
+++ crypto_monitor.py
@@ -15,7 +15,7 @@ msgr.contacts = {'chat': constants.chatid,
 msgr.broadcast_list = ['dima']
 
 cmc = CoinMarketCap(constants.COIN_MARKET_CAP_API_TOKEN)
-tokentracker = TokenTracker(constants.BSCSCAN_API_KEY))
+tokentracker = TokenTracker(constants.BSCSCAN_API_KEY)
 
 farmer = FarmChecker(msgr, cmc)
 binance_mon = BinanceCheker(msgr, constants.BSCSCAN_API_KEY, 
