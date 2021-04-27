3d4813a87a86290929948a0e9dd712dc14029370
Dima <menshikov.dmitry@gmail.com>
2021-04-27 13:18:27 +0300

    bugfix

--- crypto_monitor.py
+++ crypto_monitor.py
@@ -21,7 +21,7 @@ msgr.contacts = {'chat': constants.chatid,
 msgr.broadcast_list = ['dima']
 
 cmc = CoinMarketCap(constants.COIN_MARKET_CAP_API_TOKEN)
-tokentracker = TokenTracker(constants.BSCSCAN_API_KEY)
+tokentracker = TokenTracker()
 
 farmer = FarmChecker(msgr, cmc)
 binance_mon = BinanceCheker(msgr, constants.BSCSCAN_API_KEY, 
